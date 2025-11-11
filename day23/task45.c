77a77a0da14e55245e8107cbb0f1ae244fc3336d
TanmayJindal11 <tanmayjindal75@gmail.com>
2025-11-11 11:06:00 +0530

    day23 done

--- day23/task45.c
+++ day23/task45.c
@@ -1,4 +1,5 @@
 //Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
 #include <stdio.h>
 
 int main() {
@@ -10,13 +11,18 @@ int main() {
     printf("Enter the number of terms (n): ");
     scanf("%d", &n);
 
+    if (n <= 0) {
+        printf("Number of terms must be a positive integer.\n");
+        return 1;
+    }
+
     for (i = 0; i < n; i++) {
         sum += (double)numerator / denominator;
         numerator += 2;
         denominator += 4;
     }
 
-    printf("Sum of the series up to %d terms: %.4f\n", n, sum);
+    printf("The sum of the series up to %d terms is: %lf\n", n, sum);
 
     return 0;
 }
--- day23/task46.c
+++ day23/task46.c
@@ -1,31 +1,19 @@
-//Write a program to print the following pattern:
+/*Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
 #include <stdio.h>
 
 int main() {
-    int n, i, j, space;
-
-    printf("Enter the number of rows for the diamond: ");
-    scanf("%d", &n);
-
-    for (i = 1; i <= n; i++) {
-        for (space = 1; space <= n - i; space++) {
-            printf(" ");
-        }
-        for (j = 1; j <= 2 * i - 1; j++) {
+    int i, j;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
             printf("*");
         }
         printf("\n");
     }
-
-    for (i = n - 1; i >= 1; i--) {
-        for (space = 1; space <= n - i; space++) {
-            printf(" ");
-        }
-        for (j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-
     return 0;
 }
