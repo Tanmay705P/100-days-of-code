3e8428dcaeafca7c3c0a152287b34e79a2404e90
TanmayJindal11 <tanmayjindal75@gmail.com>
2025-11-11 11:09:57 +0530

    day25 done

---
+++ day25/task50.c
@@ -0,0 +1,24 @@
+/*Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    * */
+   #include <stdio.h>
+
+int main() {
+    int rows = 5;
+    int i, j, k;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for (k = 0; k < rows - i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
