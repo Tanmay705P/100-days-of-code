5a4040140042514913102b9d46281e960758c493
TanmayJindal11 <tanmayjindal75@gmail.com>
2025-11-11 11:26:52 +0530

    day31 done

---
+++ day31/task61.c
@@ -0,0 +1,35 @@
+//Search for an element in an array using linear search.
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element found at index: %d\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+
+    target = 60;
+    result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element found at index: %d\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
+}
