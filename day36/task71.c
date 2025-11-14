9efd8e9e975f191f7da33ca98a0b04e7d5b5fd33
TanmayJindal11 <tanmayjindal75@gmail.com>
2025-11-15 03:18:11 +0530

    day36 done

---
+++ day36/task71.c
@@ -0,0 +1,31 @@
+//Read and print a matrix.
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nMatrix elements are:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
